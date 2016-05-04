9345c617048f2591cc517860d55f57e6242ce0f1
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2016-05-04 03:05:05 +0200

    Modified test script

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 sh reset.sh
 mvn clean
 mvn install -DskipTests
-mvn test
+mvn test -DVOTING_DB_URL=localhost -DVOTING_DB_PASSWORD=root -DVOTING_DB_USER=root
 cd tests
-mvn test -Dintegrate=true
+mvn test -Dintegrate=true -DVOTING_DB_URL=localhost -DVOTING_DB_PASSWORD=root -DVOTING_DB_USER=root
