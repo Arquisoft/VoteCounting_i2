506f264a47eec32cb56f95685978effcb4baf5a7
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2016-05-04 03:18:43 +0200

    Minor fix

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+export VOTING_DB_URL=localhost
+export VOTING_DB_USER=root
+export VOTING_DB_PASSWORD=root
 sh reset.sh
 mvn clean
 mvn install -DskipTests
-mvn test -DVOTING_DB_URL=localhost -DVOTING_DB_PASSWORD=root -DVOTING_DB_USER=root
+mvn test -DVOTING_DB_URL=$VOTING_DB_URL -DVOTING_DB_PASSWORD=$VOTING_DB_PASSWORD -DVOTING_DB_USER=$VOTING_DB_USER
 cd tests
-mvn test -Dintegrate=true -DVOTING_DB_URL=localhost -DVOTING_DB_PASSWORD=root -DVOTING_DB_USER=root
+mvn test -Dintegrate=true -DVOTING_DB_URL=$VOTING_DB_URL -DVOTING_DB_PASSWORD=$VOTING_DB_PASSWORD -DVOTING_DB_USER=$VOTING_DB_USER
